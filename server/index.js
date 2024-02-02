1cde8a5b47cf19284bd6e31e3b7ea4f276e51d0a
RoshanDasan <edroshan2001@gmail.com>
2024-02-02 10:26:40 +0530

    server deploy test2

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { sendConfirmationMail } = require("./controller/sendmail");
 const { confirmEmail } = require("./controller/confirmmail");
+const cors =require('cors')
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
-const PORT = process.env.PORT;
+app.use(cors())
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send(`Welcome`));
 
